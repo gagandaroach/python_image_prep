95c766125774cf919f2cbd5ed3ba7342b58ed4c6
Gagan Daroach <daroachgb@msoe.edu>
2019-12-26 23:43:56 -0600

    actually tile the fat flipping tif with pyvips. WOOT!

--- pyvips_create_tiles.py
+++ pyvips_create_tiles.py
@@ -29,7 +29,36 @@ def create_tiles_with_vips(image_path):
     print(f'creating tiles with image: {image_path}')
     img = pyvips.Image.tiffload(image_path)
     print(img)
+    tile_name = os.path.basename(image_path)
 
+    width = img.width
+    height = img.height
+    tile_w = 1024
+    tile_h = 1024
+    
+    start_x = 10000
+    start_y = 10000
+
+    debug_count = 0
+    tiles = []
+
+    for y in range(start_x,height-tile_h,tile_h):
+        for x in range(start_y,width-tile_w,tile_w):
+            print(f'doing tile: {debug_count}')
+            #if (debug_count > 10):
+            #    return tiles
+            tile = img.extract_area(x,y,tile_w,tile_h)
+            tiles.append(tile)
+            debug_count = debug_count + 1
+
+    return tiles
+
+def save_tiles(tile_name, tiles, output_dir):
+    for index, tile in enumerate(tiles):
+        filename = f'{tile_name}_{index}.png'
+        save_path = os.path.join(output_dir, filename)
+        tile.pngsave(save_path)
+        print(f'saved tile: {index}')
 
 if __name__ == '__main__':
         
@@ -41,6 +70,13 @@ if __name__ == '__main__':
 
     filenames = load_all_img_paths(input_dir)
     #print(filenames)
-    create_tiles_with_vips(filenames[0])
+    tiles = create_tiles_with_vips(filenames[0])
 
+    tiff_name = os.path.basename(filenames[0])
+    print(f'created len{len(tiles)} tiles, now will save.')
+    save_tiles(tiff_name, tiles, output_dir)
+
+    print('done saving tiles.')
+    
+    
 
